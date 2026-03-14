370d4b7d7c11b2cff5923f666e30d717cc469ea5
jjk <kieeler456@gmail.com>
2026-03-15 08:10:25 +0900

    index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT) || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on ${PORT}`);
 });
